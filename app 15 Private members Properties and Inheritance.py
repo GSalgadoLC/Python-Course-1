e3221a17781478910f06a6745a719789bca9e8f2
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-15 23:57:11 -0700

    Add files via upload

---
+++ app 15 Private members Properties and Inheritance.py
@@ -0,0 +1,11 @@
+# -----------------------------------Private Members
+print()
+print("Private Members")
+
+
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
