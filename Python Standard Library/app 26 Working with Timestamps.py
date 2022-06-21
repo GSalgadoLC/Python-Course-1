04f2fec6433a30b6267f52d76fe8770ebe4ea644
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-20 22:32:55 -0700

    Add files via upload

---
+++ Python Standard Library/app 26 Working with Timestamps.py
@@ -0,0 +1,16 @@
+from ast import Pass
+import time
+
+print(time.time())
+
+
+def send_emails():
+    for i in range(10000):
+        pass
+
+
+start = time.time()
+send_emails()
+end = time.time()
+duration = end - start
+print(duration)
