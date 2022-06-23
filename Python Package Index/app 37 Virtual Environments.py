adbe786f6d3ec09d6b01c336d8bdc5db9d20b6f1
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-22 23:04:37 -0700

    Add files via upload

---
+++ Python Package Index/app 37 Virtual Environments.py
@@ -0,0 +1,17 @@
+# Suppose you have two applications that each require the requests package, you can use a virtual environment
+# command prompt
+# python3 -m venv
+
+# $env\bin\activate.bat
+# Pipenv
+
+# Install command promprpt run pip install pipenv
+
+# Vscode terminal run pipenv install requests
+# pipenv --venv terminal
+
+
+# pipenv shells
+
+
+# Now we can run the app.py document that requires the requests package that we downloaded on the pipenv virtual environment. If we tried to run it normally we would not have gotten correct execution because we uninstalled the requests package
