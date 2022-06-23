0c4d6c3cf2be51cac60e6e4271518a016c08d3d9
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-22 23:04:23 -0700

    Add files via upload

---
+++ Python Package Index/app 36 Pip.py
@@ -0,0 +1,14 @@
+# In this module we learned how to use pi
+# Open command prompt
+# Type pip install
+# pip list
+# pip install --upgrage
+# pip install requests
+
+
+# We can now call them in python such as
+
+import requests
+
+response = requests.get("https://google.com")
+print(response)
