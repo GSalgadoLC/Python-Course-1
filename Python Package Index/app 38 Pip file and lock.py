ed71df0233a735527058f8ae6df9adb7a0940844
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-23 03:25:30 -0700

    Add files via upload

---
+++ Python Package Index/app 38 Pip file and lock.py
@@ -0,0 +1,27 @@
+# These files are to manage dependencies
+# pipenv graph
+
+# pipenv update --outdated
+# It is important to document code so that other people know how to effectively use it
+# Docstrings is what you use
+
+"""One line description"""
+
+
+def convert():
+    """ Convert given pdf to text
+
+    Parameters:
+    path(str): The path to a pdf file
+
+    Returns:
+    str: The content of the pdf file as a text
+    """
+    print("PDF to text")
+
+
+# Pydoc math in terminal to get info about a module
+
+# To start a server in terminal
+
+# pydoc -p 1234
