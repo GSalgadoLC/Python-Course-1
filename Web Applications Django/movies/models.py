2aa2ffcf2b437c55c58033f49f5ce7842e6b854f
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-27 06:16:40 -0700

    Add files via upload

--- Web Applications Django/movies/models.py
+++ Web Applications Django/movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    # Django will automatically store genre objects in a data base
+    name = models.CharField(max_length=255)
+
+
+class Movie(models.Movie):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
