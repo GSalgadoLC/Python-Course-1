08b3d713bf46fdc82b16b57b5a5a745e16fde1f6
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-28 07:36:35 -0700

    Add files via upload

--- Web Applications Django/movies/urls.py
+++ Web Applications Django/movies/urls.py
@@ -4,11 +4,14 @@ from . import views
 # movies/
 # movies/1/details
 
+app_name = 'movies'
+
 urlpatterns = [
     # path("") represents index page, we need to map to a view function. So we need to import views from the movies directory not the vidly directory
     # views.index is simplay calling it as a reference
-    path('', views.index, name='index')
-
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail')
+    # int ensures it has to be a numeric value
 ]
 
 # We refer to the entire urlpatterns block as the url configuration
--- Web Applications Django/movies/views.py
+++ Web Applications Django/movies/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Movie
 
@@ -9,10 +9,22 @@ from .models import Movie
 
 def index(request):
     movies = Movie.objects.all()
-    output = ', '.join([m.title for m in movies])
+    #output = ', '.join([m.title for m in movies])
     # SELECT * FROM movies_movies
     # Movie.objects.filter(release_year=1984)
     # SELECT * FROM movies_movie WHERE  release_year=1984
     # Single object
     # Movie.objects.get(id=1)
-    return HttpResponse(output)
+   # return HttpResponse(output)
+    return render(request, 'movies/index.html', {'movies': movies})
+
+    # For each movie added we want to render a table row
+    # We use double curly and django also has powerful code snppets
+
+    # As best practice we do not want a generic name index.html. We create a namespace movies inside templates folder of app movies and move index.html inside
+
+
+def detail(request, movie_id):
+
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
