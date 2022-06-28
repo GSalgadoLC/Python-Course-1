cac214a315878b239ef2134d517ea23a47bbcd40
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-28 01:04:08 -0700

    Add files via upload

--- Web Applications Django/movies/admin.py
+++ Web Applications Django/movies/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Genre, Movie
 # Register your models here.
 
-admin.site.register(Genre)
-admin.site.register(Movie)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class MovieAdmin(admin.ModelAdmin):
+   # fields = ('title', ) can specify what you want to show or you can use exclude certain field
+    exclude = ('date_created', )
+    list_display = ('title', 'number_in_stock', 'daily_rate', )
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
--- Web Applications Django/movies/models.py
+++ Web Applications Django/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,6 +8,12 @@ class Genre(models.Model):
     # Django will automatically store genre objects in a data base
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -14,3 +21,6 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
+
+# We are simly applying a reference not calling it because then the current datetime would be hardcoded
