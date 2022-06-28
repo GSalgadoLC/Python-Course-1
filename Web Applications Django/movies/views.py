0e1a1d426e9a371737b33cfcf949625c39377959
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-28 01:36:59 -0700

    Add files via upload

--- Web Applications Django/movies/views.py
+++ Web Applications Django/movies/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
+from .models import Movie
+
 # Create your views here.
+
+
+def index(request):
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+    # SELECT * FROM movies_movies
+    # Movie.objects.filter(release_year=1984)
+    # SELECT * FROM movies_movie WHERE  release_year=1984
+    # Single object
+    # Movie.objects.get(id=1)
+    return HttpResponse(output)
