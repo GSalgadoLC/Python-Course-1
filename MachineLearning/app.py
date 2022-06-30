da25f3a791d8459836a0facfce862186543c7ed9
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-06-29 23:59:43 -0700

    Add files via upload

---
+++ MachineLearning/app.py
@@ -0,0 +1,17 @@
+# 1Import the data
+# 2Clean the data
+# 3Split the data in trainging and test sets
+# 4Create a model
+# 5 Train the model
+# 6 Make predictions
+# 7Evaluate and improve
+
+# Popular libraries
+#  NumPy - multidimensional arrays
+# Pandas - rows columns
+# MatPlotLib - plotting graphs
+# Scikit-learn ml and algos
+
+
+# Environment we use Jupyter easier to visualize and inspect data
+# We download from Anaconda Distribution
